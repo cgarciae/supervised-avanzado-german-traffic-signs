31c07484331ce0583363c3faa335c12bf33bc9cf
cgarciae <cgarcia.e88@gmail.com>
2017-06-26 16:39:43 -0500

    save

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def main(device, epochs, batch_size):
 
     # obtener todas las imagenes (lento)
     data_generator = dataset.training_set.random_batch_arrays_generator(batch_size)
-    # data_generator = utils.batch_random_image_rotation(data_generator, 15.0)
+    data_generator = utils.batch_random_image_rotation(data_generator, 15.0)
     data_generator = cz.map(Dict(features = P[0], labels = P[1]), data_generator)
 
     graph = tf.Graph()
